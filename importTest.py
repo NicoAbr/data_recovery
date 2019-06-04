bc9b95b30897c5e200968381d59181a059511304
Robert Schirm <robert.schirm@student.jade-hs.de>
2019-06-04 23:42:33 +0200

    erste .wav Datei wird gefunden und geschrieben

--- importTest.py
+++ importTest.py
@@ -1,10 +1,11 @@
 import pathlib
-wav = pathlib.Path('test.wav')
+wav = pathlib.Path('data_deleted.img')
 
 with wav.open('rb') as file:
-    filetype = file.read(4)
-    filelen = file.read(4)
-    rifftype = file.read(4)
+   filetype = file.read(4) 
+   filelen = file.read(4)
+   #filelen = int.from_bytes(file.read(4), "little")
+   rifftype = file.read(4)
     
 print('filetype:', filetype)
 print('filelen:', filelen)
