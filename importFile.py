1f2add6a3a7ab3dea836d5b6eb28a79215451fdb
Nico Abraham <nico.abraham@student.jade-hs.de>
2019-06-05 17:12:17 +0200

    findet jetzt alle datatype indices

--- importFile.py
+++ importFile.py
@@ -1,10 +1,32 @@
 import pathlib
+import numpy as np
 hd = pathlib.Path('data_deleted.img')
+
 
 with hd.open('rb') as file:
 	hdData = file.read()
 
+
+
 idx = hdData.index(b'WAVE')
+print(idx)
+
+# find more than one index (from: https://stackoverflow.com/questions/...
+# 			6294179/how-to-find-all-occurrences-of-an-element-in-a-list)
+def indices(lst, element):
+    result = []
+    offset = -1
+    while True:
+        try:
+            offset = lst.index(element, offset+1)
+        except ValueError:
+            return result
+        result.append(offset)
+
+# using function to get all WAVE header
+idx_list = indices(hdData, b'WAVE')
+print(idx_list)
+
 
 filetype = hdData[(idx-8):(idx-4)]
 filelen = int.from_bytes(hdData[(idx-4):idx], "little")
