dac11d06ba2f00ab49cab56abc78072db3b9bf12
Tilljan Jansohn <tilljan@jansohn-hue.de>
2019-06-09 15:36:04 +0200

    Funktion zum findes JPG Endes

--- functions.py
+++ functions.py
@@ -9,3 +9,22 @@ def getIdx(lst, element):
         except ValueError:
             return result
         result.append(offset)
+		
+		
+
+def getJpgEof(lst, startIdx, idxCounter):
+marker = []
+	file.seek(idx_list[1])
+	while marker != 0xff:
+		marker = file.read(1)
+		idxCounter += 1
+	if file.read(1) != (0x00 or 0x01 or 0xd0 or 0xd1 or 0xd2 or 0xd3 or 0xd4 or 0xd5 or 0xd6 or 0xd7 or 0xd8):
+		blockLength = int.from_bytes(file.read(2), "big")
+		idxCounter += 2
+	else:
+		marker = []
+		while marker != 0xff:
+			marker = file.read(1)
+			idxCounter += 1
+	file.read(blockLength-2)
+	idxCounter += blockLength-2
--- jpgFinder.py
+++ jpgFinder.py
@@ -23,28 +23,32 @@ hd = pathlib.Path('data_deleted.img')
 with hd.open('rb') as file:
 	hdData = file.read()
 
-headerIdx = []
-for idx in range(len(hdData)):
-	if (hdData[idx] == 0xff and 
-		hdData[idx+1] == 0xd8 and
-		hdData[idx+2] == 0xff and
-		hdData[idx+3] == 0xe0 and
-		hdData[idx+4] == 0x00 and
-		hdData[idx+5] == 0x10 and
-		hdData[idx+6] == 0x4a and
-		hdData[idx+7] == 0x46 and
-		hdData[idx+8] == 0x49 and
-		hdData[idx+9] == 0x46 and
-		hdData[idx+10] == 0x00):
-		headerIdx.append(idx)
-		
-print(headerIdx)
+#headerIdx = []
+#for idx in range(len(hdData)):
+#	if (hdData[idx] == 0xff and 
+#		hdData[idx+1] == 0xd8 and
+#		hdData[idx+2] == 0xff and
+#		hdData[idx+3] == 0xe0 and
+#		hdData[idx+4] == 0x00 and
+#		hdData[idx+5] == 0x10 and
+#		hdData[idx+6] == 0x4a and
+#		hdData[idx+7] == 0x46 and
+#		hdData[idx+8] == 0x49 and
+#		hdData[idx+9] == 0x46 and
+#		hdData[idx+10] == 0x00):
+#		headerIdx.append(idx)
+#		
+#print(headerIdx)
 
 # using function to get all WAVE header
 idx_list = f.getIdx(hdData, b'JFIF')   
-print(idx_list)
-
-print(len(idx_list))
-print(len(headerIdx))
 
 
+marker = []
+idxCounter = 0
+f.getJpgEof(hdData, idx_list[1], idxCounter)
+
+	
+	
+	
+	
