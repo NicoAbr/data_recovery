b9ec556e08894819474cca4fea48bcbbff94353d
Tilljan Jansohn <tilljan@jansohn-hue.de>
2019-06-11 21:09:57 +0200

    Etwas aufgeräumt. Funktioniert leider immer noch nicht 100%ig

--- functions.py
+++ functions.py
@@ -14,8 +14,6 @@ def getIdx(lst, element):
 	
 # Informationen dazu unter: https://stackoverflow.com/questions/4585527/detect-eof-for-jpg-images?answertab=votes#
 # Funktion zum suchen der EOF Indizes. 
-# funktioniert nur leider noch nicht ganz (Die Ausgespuckten Werte ergeben keinen Sinn)
-# Übergeben wird die Datei und der Indize vom JPG Header
 def getJpgEof(dataPath, currentIdx):
 
 	with dataPath.open('rb') as file:
@@ -23,8 +21,6 @@ def getJpgEof(dataPath, currentIdx):
 		file.seek(currentIdx)
 		marker1 = file.read(1)
 		marker2 = file.read(1)
-#		print(marker1)
-#		print(marker2)
 		currentIdx += 2
 				
 		if (marker1 == b'\xff' and marker2 == b'\xd9'):
@@ -32,17 +28,11 @@ def getJpgEof(dataPath, currentIdx):
 			
 		elif (marker1 == b'\xff' and marker2 != b'\xda'):
 			segmentLen = int.from_bytes(file.read(2), "big")
-#			print(currentIdx)
-#			print(segmentLen)
 			currentIdx += segmentLen
-#			print(currentIdx)
 			currentIdx = getJpgEof(dataPath, currentIdx)
-#			print(currentIdx)
-#			print("Ich bin hier")
+
 			
 		elif (marker1 == b'\xff' and marker2 == b'\xda'):
-#			print(currentIdx)
-			file.seek(currentIdx)	
 			marker1 = 0
 			marker2 = 0
 		
@@ -56,75 +46,33 @@ def getJpgEof(dataPath, currentIdx):
 #			currentIdx = getJpgSos(dataPath, currentIdx)
 			marker1 = file.read(1)
 			marker2 = file.read(1)
-#			print(marker1)
-#			print(marker2)
-#			print("Ich bin hoffentlich fertig")
-#			print(currentIdx)
 			segmentLen = int.from_bytes(file.read(2), "big")
 			currentIdx += segmentLen
-#			print(currentIdx)
 			currentIdx = getJpgEof(dataPath, currentIdx)
-#		print(currentIdx)
-#		print("error")
-#		print(" ")
-		
+	
 		return currentIdx	
 
-def getJpgSos(dataPath, currentIdx):
-	with dataPath.open('rb') as file:
-	
-		file.seek(currentIdx)	
-		marker1 = 0
-		marker2 = 0
-		
-		while marker1 != b'\xff' or marker2 == (b'\x00' or b'\x01' or b'\xd0' or b'\xd1' or 
-												b'\xd2' or b'\xd3' or b'\xd4' or b'\xd5' or 
-												b'\xd6' or b'\xd7' or b'\xd8'):
-			marker1 = file.read(1)
-			marker2 = file.read(1)
-			currentIdx += 1	
-			file.seek(currentIdx)
-			
+#def getJpgSos(dataPath, currentIdx):
+#	with dataPath.open('rb') as file:
+#	
+#		file.seek(currentIdx)	
+#		marker1 = 0
+#		marker2 = 0
+#		
+#		while marker1 != b'\xff' or marker2 == (b'\x00' or b'\x01' or b'\xd0' or b'\xd1' or 
+#												b'\xd2' or b'\xd3' or b'\xd4' or b'\xd5' or 
+#												b'\xd6' or b'\xd7' or b'\xd8'):
+#			marker1 = file.read(1)
+#			marker2 = file.read(1)
+#			currentIdx += 1	
+#			file.seek(currentIdx)
+#			
 #		marker2 = file.read(1)
 #		currentIdx += 1
 #		if marker2 == 
 #			print(currentIdx)
-			currentIdx = getJpgSos(dataPath, currentIdx)
+#			currentIdx = getJpgSos(dataPath, currentIdx)
 #		print("Hier bin ich")
 #		print(marker1)
 #		print(marker2)		
-		return currentIdx
-	
-	
-	
-	
-	
-#	marker = 0
-#	# sucht ab dem übergebenen JPG Header bis er auf 0xff (255) stößt
-#	while marker != 0xff:
-#		marker = lst[currentIdx+1]
-#		currentIdx += 1
-#		
-#	# wenn er nach dem 0xff auf 0xd9 stößt ist der EOF gefunden
-#	if lst[currentIdx+1] == 0xd9:
-#		currentIdx += 1
-#		return currentIdx
-#	# wenn nach dem 0xff keiner der Marker 0xd0 bis 0xd9 auftaucht
-#	# sind die nächsten beiden Bytes die Längenangabe für den Block
-#	# Um diese Länge wird dann nach forne geskipt und die funktion mit der
-#	# aktualisierten Indizierung neu aufgerufen (rekursiv)
-#	elif lst[currentIdx+1] != (0x00 or 0x01 or 0xd0 or 0xd1 or 
-#							   0xd2 or 0xd3 or 0xd4 or 0xd5 or 
-#							   0xd6 or 0xd7 or 0xd8):
-#		currentIdx += 1
-#		x = lst[currentIdx+1]
-#		y = lst[currentIdx+2]
-#		blockLength =  y*256 + x
-#		currentIdx += blockLength
-#		currentIdx = getJpgEof(lst, currentIdx)
-#		return currentIdx
-#	# wenn der Marker 0xd0 bis 0xd9 oder 0x00 ist, dann sucht er nach dem nächsten 
-#	# 0xff indem die funktion mit den aktualisierten marker aufgerufen wird.
-#	else:
-#		currentIdx = getJpgEof(lst, currentIdx)
 #		return currentIdx
--- jpgFinder.py
+++ jpgFinder.py
@@ -1,35 +1,12 @@
 # Find JPG
 import pathlib
 import functions as f
-import numpy as np
-
 
 runNo = 1
 hd = pathlib.Path('data_deleted.img')
 
 with hd.open('rb') as file:
 	hdData = file.read()
-
-
-
-# Damit könnte man den kompletten Header finden, dauert aber ziemlich lange	
-#headerIdx = []
-#for idx in range(len(hdData)):
-#	if (hdData[idx] == 0xff and 
-#		hdData[idx+1] == 0xd8 and
-#		hdData[idx+2] == 0xff and
-#		hdData[idx+3] == 0xe0 and
-#		hdData[idx+4] == 0x00 and
-#		hdData[idx+5] == 0x10 and
-#		hdData[idx+6] == 0x4a and
-#		hdData[idx+7] == 0x46 and
-#		hdData[idx+8] == 0x49 and
-#		hdData[idx+9] == 0x46 and
-#		hdData[idx+10] == 0x00):
-#		headerIdx.append(idx)
-#		
-#print(headerIdx)
-
 
 
 # using function to get all JPG header
@@ -53,17 +30,4 @@ for element in idx_list:
 			with new_file.open('wb') as file:
 				file.write(data)
 			
-
-			
-
-
-
-# alle Indizes werden in die Funktion gegeben und der entsprechnende
-# EOF Indize kommt zurück (EOF steht für "end of file")
-#for element in idx_list:
-#	element -= 6;
-#	
-#	print(element)
-#	print(f.getJpgEof(hdData, element))
-#	print(" ")
 
