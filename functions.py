602b28ca60cdb74bec30d2019157e9560d115840
Tilljan Jansohn <tilljan@jansohn-hue.de>
2019-06-12 02:10:16 +0200

    Auskommentierung bei den Funktionen

--- functions.py
+++ functions.py
@@ -16,26 +16,33 @@ def getIdx(lst, element):
 		
 
 def getJpgEof(dataPath, currentIdx):
-
+	# going to the start of the header 
+	# and read in markers
 	with dataPath.open('rb') as file:
 		file.seek(currentIdx)
 		marker1 = file.read(1)
 		marker2 = file.read(1)
 		currentIdx += 2
-				
+		
+		# check for end of file markers
 		if (marker1 == b'\xff' and marker2 == b'\xd9'):
 			return currentIdx
-			
+		
+		# preclude SOS markers
 		elif (marker1 == b'\xff' and marker2 != b'\xda'):
+			# read in block length
 			segmentLen = int.from_bytes(file.read(2), "big")
 			currentIdx += segmentLen
+			# recall getJpgEof function
 			currentIdx = getJpgEof(dataPath, currentIdx)
 			return currentIdx
-			
+		
+		# check for SOS markers
 		elif (marker1 == b'\xff' and marker2 == b'\xda'):
 			marker1 = 0
 			marker2 = 0
-		
+			
+			# skip forward to the next block header
 			while marker1 != b'\xff' or marker2 == (b'\x00' or b'\x01' or b'\xd0' or b'\xd1' or 
 													b'\xd2' or b'\xd3' or b'\xd4' or b'\xd5' or 
 													b'\xd6' or b'\xd7' or b'\xd8'):
@@ -43,16 +50,21 @@ def getJpgEof(dataPath, currentIdx):
 				marker2 = file.read(1)
 				currentIdx += 1	
 				file.seek(currentIdx)
+			# read in block header markers
 			marker1 = file.read(1)
 			marker2 = file.read(1)
+			# read in block length
 			segmentLen = int.from_bytes(file.read(2), "big")
 			currentIdx += segmentLen
+			# recall getJpgEof function
 			currentIdx = getJpgEof(dataPath, currentIdx)	
 		
+		# skip forward to end of file markers
 		while marker1 != b'\xff' or marker2 != b'\xd9':
 			marker1 = file.read(1)
 			marker2 = file.read(1)
 			currentIdx += 1	
 			file.seek(currentIdx)
-	
+		
+		# return end of file index
 		return currentIdx+1
